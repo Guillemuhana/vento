163a2c6ebcd23c5756c15fe5824d2b0c56165bee
Guille <guille@studiob2b.dev>
2026-09-16 14:16:52 -0300

    Use Fiorito menu images

--- supabase/migrations/005_fiorito_menus.sql
+++ supabase/migrations/005_fiorito_menus.sql
@@ -68,3 +68,19 @@ where lower(s.name) = 'fiorito almacen'
     select 1 from products existing
     where existing.store_id = s.id and lower(existing.name) = lower(p.name)
   );
+
+update stores
+set cover_url = 'https://tb-static.uber.com/prod/image-proc/processed_images/384982d20c90ce05551ea6d0a7f24346/f6deb0afc24fee6f4bd31a35e6bcbd47.jpeg'
+where lower(name) = 'fiorito';
+
+update products
+set image_url = 'https://tb-static.uber.com/prod/image-proc/processed_images/384982d20c90ce05551ea6d0a7f24346/783282f6131ef2258e5bcd87c46aa87e.jpeg'
+where store_id in (select id from stores where lower(name) = 'fiorito');
+
+update stores
+set cover_url = 'https://tb-static.uber.com/prod/image-proc/processed_images/9201f1f5a0bd5538a96dfa621c084958/19ec62ba51fde35ba0aff5b84321c5af.jpeg'
+where lower(name) = 'fiorito almacen';
+
+update products
+set image_url = 'https://tb-static.uber.com/prod/image-proc/processed_images/9201f1f5a0bd5538a96dfa621c084958/4bee0baf259f2b3c95aacdebea87e950.jpeg'
+where store_id in (select id from stores where lower(name) = 'fiorito almacen');
